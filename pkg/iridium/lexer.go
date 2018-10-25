20601cb73393abbfb3aa31f0428d6c612c9fa73d
dancing-koala <ccbbruneau@gmail.com>
2018-10-25 03:52:49 +0200

    Add lexer functions for registers & int operands

--- pkg/iridium/lexer.go
+++ pkg/iridium/lexer.go
@@ -10,6 +10,7 @@ const (
 	opcodeToken lexer.TokenType = iota
 	registerToken
 	intOperandToken
+	numberToken
 )
 
 func lex(src string) {
@@ -47,13 +48,63 @@ func whitespaceState(l *lexer.L) lexer.StateFunc {
 
 	r = l.Peek()
 
-	if isUpperAlpha(r) {
+	switch {
+	case isUpperAlpha(r):
 		return opcodeState
+
+	case isRegisterSymbol(r):
+		return registerState
+
+	case isIntOperandSymbol(r):
+		return intOperandState
 	}
 
 	return nil
 }
 
+func registerState(l *lexer.L) lexer.StateFunc {
+	r := l.Next()
+
+	if !isRegisterSymbol(r) {
+		l.Error(fmt.Sprintf("Not a register symbol: %q", r))
+		return nil
+	}
+
+	l.Ignore() //Drop the symbol
+	l.Take("0123456789")
+
+	l.Emit(registerToken)
+
+	return whitespaceState
+}
+
+func intOperandState(l *lexer.L) lexer.StateFunc {
+	r := l.Next()
+
+	if !isIntOperandSymbol(r) {
+		l.Error(fmt.Sprintf("Not an int operand symbol: %q", r))
+	}
+
+	l.Ignore() // Drop the # symbol
+	l.Take("0123456789")
+
+	l.Emit(intOperandToken)
+
+	return whitespaceState
+}
+
 func isUpperAlpha(r rune) bool {
 	return r >= 'A' && r <= 'Z'
+}
+
+func isNumber(r rune) bool {
+	return r >= '0' && r <= '9'
+}
+
+func isRegisterSymbol(r rune) bool {
+	return r == '$'
+}
+
+func isIntOperandSymbol(r rune) bool {
+	return r == '#'
 }
